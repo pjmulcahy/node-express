cc0ba4d5d35c3c277cc2e1679c657f5817b723be
Patrick Mulcahy <pjmulcahy@gmail.com>
2021-11-04 15:22:14 -0500

    prep for workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,39 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+//below is for workshop
+
+/* app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+}); */
+
+//above is for workshop
+
 module.exports = campsiteRouter;
